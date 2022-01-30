8757e4cb4f9932d01c58c53c4091cfaea1ed636c
Jonathan Zhao <jonathanzhaon@gmail.com>
2022-01-30 15:27:08 -0500

    Initial Commit

---
+++ Shared/Pose_Estimation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pose_Estimation_AppApp.swift
+//  Shared
+//
+//  Created by Jonathan Zhao on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct Pose_Estimation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
