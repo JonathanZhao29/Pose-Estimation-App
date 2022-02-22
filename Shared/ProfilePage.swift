09d31b27e506f300c8d56014c378457eaa457a44
Jonathan Zhao <jonathanzhaon@gmail.com>
2022-02-22 15:09:01 -0500

    Added button functionality to profile page
    Created variables to link to future backend info, such as name and description. My Posts tab and Favorites tab now toggle on and off according to what tab user is on. Added debug prints to show active tab.

--- Shared/ProfilePage.swift
+++ Shared/ProfilePage.swift
@@ -8,33 +8,78 @@
 import SwiftUI
 
 struct ProfilePage: View {
+    
+    //For connection with database
+    //Data will change based on user info
+    var profileName:String = "Jonathan Zhao"
+    var profileDescription:String = "Ice dancer turned CS major. Yep, that's me. You're probably wondering how I got here."
+    
+    
+    //For changing tabs
+    @State var tab:Bool = true
+    @State var myPostsImageName:String = "photo.fill"
+    @State var favoritesImageName:String = "star"
+    
+    //Current tab debug variable
+    @State var currentTab = "myPosts"
+    
     var body: some View {
         VStack {
             HStack{
                 //Edit Profile Button
                 Spacer()
+                Button{
+                    print("Edit Profile Button Pressed")
+                } label:{
                 Image(systemName: "pencil")
                     .padding()
+                }
             }
             //Profile Picture with circle crop
             Circle()
                 .frame(width: 100, height: 100)
                 //.fill(Color.red)
             //Username
-            Text("**Jonathan Zhao**")
+            Text("**\(profileName)**")
                 .font(.title2)
             //User Description
-            Text("Ice dancer turned CS major. Yep, that's me. You're probably wondering how I got here.")
+            Text(profileDescription)
+                .padding()
             
             //Tabs for my posts/saved
             HStack {
                 //My Posts
-                Image(systemName: "photo.fill")
+                
+                Button{
+                    if tab != true{
+                            tab.toggle()
+                            currentTab = "My Posts"
+                    }
+                    myPostsImageName = tab == true ? "photo.fill" : "photo"
+                    favoritesImageName = tab == true ? "star" : "star.fill"
+                    print("My Posts Tab Pressed")
+                    print("Current tab:\(currentTab)")
+                } label:{
+                Image(systemName: myPostsImageName)
                     .padding()
+                }
                 Spacer()
                 //Saved Favorites
-                Image(systemName: "star.fill")
+                Button{
+                    if tab != false{
+                            tab.toggle()
+                            currentTab = "Favorites"
+                            
+                    }
+                    favoritesImageName = tab == true ? "star" : "star.fill"
+                    myPostsImageName = tab == true ? "photo.fill" : "photo"
+                    
+                    print("Favorites Tab Pressed")
+                    print("Current tab:\(currentTab)")
+                } label:{
+                Image(systemName: favoritesImageName)
                     .padding()
+                }
             }
             //Video Posts, changes with tab
             VStack{
@@ -48,7 +93,6 @@ struct ProfilePage: View {
                     .padding()
             }
             
-            //Area for video posts
             Spacer()
         }
     }
